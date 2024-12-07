7a1f3af30a3bd8650d96a3df4a31ed4997b38598
art-idea5642 <chertezhy.ru@gmail.com>
2024-12-07 16:23:47 +0400

    Сделано Дз

---
+++ src/main/java/com/art/hogwards_students/controllers/StudentController.java
@@ -0,0 +1,66 @@
+package com.art.hogwards_students.controllers;
+
+import com.art.hogwards_students.model.Student;
+import com.art.hogwards_students.services.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    public StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentInfo(@PathVariable long id) {
+        Student student = studentService.findStudent(id);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @GetMapping("/age/{age}")
+    public ResponseEntity <List <Student>> getStudentsByAge (@PathVariable int age) {
+        List<Student> students = studentService.findStudentByAge(age);
+        if (students.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(students);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Collection<Student>> getAllStudents() {
+        return ResponseEntity.ok(studentService.getAll());
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @PutMapping
+    public ResponseEntity<Student> editStudent(@RequestBody Student student) {
+        Student editableStudent = studentService.editStudent(student);
+        if (editableStudent == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(editableStudent);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable long id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/com/art/hogwards_students/services/StudentService.java
@@ -0,0 +1,42 @@
+package com.art.hogwards_students.services;
+
+import com.art.hogwards_students.model.Student;
+import com.art.hogwards_students.repositories.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student findStudent(long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public List<Student> findStudentByAge (int age) {
+        return studentRepository.findByAge(age);
+    }
+
+    public Student editStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> getAll() {
+        return studentRepository.findAll();
+    }
+}
